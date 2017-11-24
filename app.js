aedcfd8be67f870a99f18073817cdd3b66b70f40
antoinecaron <antoinecaron.simplon@gmail.com>
2017-11-24 15:31:58 +0100

    exo_progress

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // good luck !
+$('input').css("background", function(){
+	return $(this).data("color");
+});
+
+$("input").click(function(){
+	var color = $(this).data("color");
+	$("body").css("background", color);
+});
+
+var checked = $('modify-text').is(':checked');
+	if{
+		
+	}
